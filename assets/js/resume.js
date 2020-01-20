409fe9965f02c2f94217c0e337f76c077c92b3b6
Michael Denman <denmanmd@gmail.com>
2020-01-20 11:45:03 -0600

    Correct crossover between all and individual

---
+++ assets/js/resume.js
@@ -0,0 +1,8 @@
+function showHide(bullet) {
+    var bulletId = document.getElementById(bullet);
+        if (bulletId.style.display !== "none") {
+            bulletId.style.display = "none";
+        } else {
+            bulletId.style.display = "list-item";
+        }
+    }
